c994c05f6ae3981e1f479039cacd56b2d2324e38
Simon Mutch <smutch@unimelb.edu.au>
2020-12-16 09:44:52 +1100

    Day 3 - part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -11,24 +11,28 @@ fn main() {
     let args = Cli::from_args();
     let input = std::fs::read_to_string(args.input).expect("Failed to read input file.");
 
-    // let height = input.lines().count();
     let width = input.lines().next().unwrap().len();
-    let mut pos = (0usize, 0usize);
-    let movement = (3i16, 1i16);
+    let movements = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let mut product = 1u64;
 
-    let ntrees = input
-        .lines()
-        .step_by(movement.1 as usize)
-        .fold(0, |count, line| {
-            println!("{},{} -> {}", pos.0, pos.1, line.chars().nth(pos.0).unwrap());
-            let mut new_count = count;
-            if line.chars().nth(pos.0).unwrap() == '#' {
-                new_count += 1;
-            }
-            pos.0 = (pos.0 + movement.0 as usize) % width;
-            pos.1 += movement.1 as usize;
-            new_count
-        });
+    for movement in movements.iter() {
+        let mut pos = (0usize, 0usize);
+        let ntrees = input
+            .lines()
+            .step_by(movement.1 as usize)
+            .fold(0, |count, line| {
+                let mut new_count = count;
+                if line.chars().nth(pos.0).unwrap() == '#' {
+                    new_count += 1;
+                }
+                pos.0 = (pos.0 + movement.0 as usize) % width;
+                pos.1 += movement.1 as usize;
+                new_count
+            });
+    
+        println!("Right {}, down {} -> Hit {} trees!", movement.0, movement.1, ntrees);
+        product *= ntrees;
+    }
 
-    println!("Hit {} trees!", ntrees);
+    println!("product = {}", product);
 }
